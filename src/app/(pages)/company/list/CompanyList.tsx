a7c4ce60fbdb582f98d4734474c97fa90c2b1c56
Phạm Quang Trường <truongpham090325@gmail.com>
2026-03-10 10:14:57 +0700

    Tính năng Phân trang (Trang Danh sách công ty)

--- src/app/(pages)/company/list/CompanyList.tsx
+++ src/app/(pages)/company/list/CompanyList.tsx
@@ -1,20 +1,40 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 "use client";
 import { CardCompanyItem } from "@/app/components/card/CardCompanyItem";
+import { useRouter, useSearchParams } from "next/navigation";
 import { useEffect, useState } from "react";
 
 export const CompanyList = () => {
+  const searchParams = useSearchParams();
   const [companyList, setCompanyList] = useState<any[]>([]);
+  const [totalPage, setTotalPage] = useState(0);
+  const router = useRouter();
+  const page = searchParams.get("page") || "";
 
   useEffect(() => {
-    fetch(`${process.env.NEXT_PUBLIC_API_URL}/company/list?limitItems=3`, {
-      method: "GET",
-    })
+    fetch(
+      `${process.env.NEXT_PUBLIC_API_URL}/company/list?limitItems=3&page=${page}`,
+      {
+        method: "GET",
+      },
+    )
       .then((res) => res.json())
       .then((data) => {
         setCompanyList(data.companyList);
+        setTotalPage(data.totalPage);
       });
-  }, []);
+  }, [page]);
+
+  const handlePagination = (event: any) => {
+    const value = event.target.value;
+    const params = new URLSearchParams(searchParams.toString());
+    if (value) {
+      params.set("page", value);
+    } else {
+      params.delete("page");
+    }
+    router.push(`?${params.toString()}`);
+  };
 
   return (
     <>
@@ -26,12 +46,16 @@ export const CompanyList = () => {
 
       <div className="mt-[30px]">
         <select
-          name=""
+          onChange={handlePagination}
           className="border border-[#DEDEDE] rounded-[8px] py-[12px] px-[18px] font-[400] text-[16px] text-[#414042] outline-none"
         >
-          <option value="">Trang 1</option>
-          <option value="">Trang 2</option>
-          <option value="">Trang 3</option>
+          {Array(totalPage)
+            .fill("")
+            .map((item, index) => (
+              <option key={index} value={index + 1}>
+                Trang {index + 1}
+              </option>
+            ))}
         </select>
       </div>
     </>
